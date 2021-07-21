9dcefcf5cd5f651dec122ceb8c6d30b0f38163e9
Anna Medyukhina <37274810+amedyukhina@users.noreply.github.com>
2021-07-21 11:51:35 -0500

    demo script with example commands

---
+++ demo.sh
@@ -0,0 +1,2 @@
+python care_batch/generate_training_data.py --basepath /research/sharedresources/cbi/common/Anna/test_data/CARE/train --source_dir low --target_dir GT --save_file /home/amedyukh/data.npz --patch_size 32,64,64 --n_patches_per_image 20 --axes ZYX
+
