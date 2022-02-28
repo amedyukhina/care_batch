744086f558eaadb38a6e36455adc4fc2877ff382
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-02-28 13:23:18 -0600

    plot ssim maps + accuracy measures

--- care_batch/plot.py
+++ care_batch/plot.py
@@ -6,12 +6,15 @@ from csbdeep.io import load_training_data
 from csbdeep.models import CARE
 from csbdeep.utils import plot_some
 from skimage import io
+from .evaluate import nrmse
+from skimage.metrics import structural_similarity
 
 
-def plot_pairs(fn, folders, basepath, figsize=7, pmin=2, pmax=99.8):
+def plot_pairs(fn, folders, basepath, figsize=7, pmin=0, pmax=100,
+               plot_ssim=False, name_high='high'):
     imgs = [io.imread(os.path.join(basepath, fld, fn)) for fld in folders]
     title = ''
-    plt.figure(figsize=(figsize * len(imgs), figsize))
+    ts = 0
     j = 0
     for i in range(len(folders)):
         if len(folders[i]) > 20:
@@ -19,10 +22,25 @@ def plot_pairs(fn, folders, basepath, figsize=7, pmin=2, pmax=99.8):
             code = rf'model {j}'
             title += f"{code}: {folders[i]}\n"
             folders[i] = code
+            ts += 1
 
+    plt.figure(figsize=(figsize * len(imgs), figsize + ts))
     plot_some(np.stack(imgs), title_list=[folders], pmin=pmin, pmax=pmax)
     plt.suptitle(title)
     plt.tight_layout()
+
+    if plot_ssim:
+        ssim_maps = []
+        titles = []
+        gt = io.imread(os.path.join(basepath, name_high, fn))
+        for img in imgs:
+            ssim_ind, ssim_map = structural_similarity(gt, img, full=True)
+            ssim_maps.append(ssim_map)
+            titles.append(rf'SSIM={round(ssim_ind, 2)}; NRSME={round(nrmse(gt, img), 4)}')
+
+        plt.figure(figsize=(figsize * len(imgs), figsize))
+        plot_some(np.stack(ssim_maps), title_list=[titles], pmin=0, pmax=100)
+        plt.tight_layout()
 
 
 def plot_patches(datafile, ncols=7, nrows=2, figsize=4, model_name=None, model_basedir=None):
